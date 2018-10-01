805f5ee3b9e21017c254200494887f88de60b418
Sergey Bubeev <bubeev@magora-systems.com>
2018-10-01 18:26:29 +0700

    Initial commit

---
+++ app/containers/HomePage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/* eslint-disable react/prefer-stateless-function */
+export default class HomePage extends React.PureComponent {
+  render() {
+    return (
+      <h1>
+        home page
+      </h1>
+    );
+  }
+}
